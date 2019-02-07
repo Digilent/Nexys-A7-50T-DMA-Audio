9690da4ff8bf6183d90c561f6fb5118681be19d5
ArtVVB <arthur.brown@digilent.com>
2019-02-07 10:09:16 -0800

    fix incorrect uart baud rate

--- src/bd/design_1.tcl
+++ src/bd/design_1.tcl
@@ -522,7 +522,7 @@ proc create_root_design { parentCell } {
   # Create instance: axi_uartlite_0, and set properties
   set axi_uartlite_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_uartlite:2.0 axi_uartlite_0 ]
   set_property -dict [ list \
-   CONFIG.C_BAUDRATE {460800} \
+   CONFIG.C_BAUDRATE {230400} \
    CONFIG.UARTLITE_BOARD_INTERFACE {usb_uart} \
  ] $axi_uartlite_0
 
